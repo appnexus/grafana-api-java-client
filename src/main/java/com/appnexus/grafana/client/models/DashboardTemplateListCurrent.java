961de447d5e028351dad5c6c7d6fc90f54521cfd
bascarsija <bascarsija.dev@gmail.com>
2018-02-06 22:28:37 -0800

    value should be String in DashboardTemplateListCurrent

--- src/main/java/com/appnexus/grafana/client/models/DashboardTemplateListCurrent.java
+++ src/main/java/com/appnexus/grafana/client/models/DashboardTemplateListCurrent.java
@@ -6,13 +6,11 @@ import lombok.EqualsAndHashCode;
 import lombok.ToString;
 import lombok.experimental.Accessors;
 
-import java.util.List;
-
 @Data
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
 @Accessors(fluent = true)
 public class DashboardTemplateListCurrent extends FlexibleSchemaComponent {
   private String text;
-  private List<String> value;
+  private String value;
 }
